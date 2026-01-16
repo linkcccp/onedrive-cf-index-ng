89e95340613ac31f651808538bb95c934ddc6ae2
linkccccp <linkzengyaoxiang@outlook.com>
2026-01-16 16:40:07 +0800

    fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 const runtimeCaching = require('next-pwa/cache')
 const withPWA = require('next-pwa')({
   dest: 'public',
-  runtimeCaching
+  runtimeCaching: [
+    {
+      urlPattern: /^https:\/\/static\.cloudflareinsights\.com\//,
+      handler: 'NetworkOnly',
+    },
+    ...runtimeCaching,
+  ],
 })
 
 module.exports = withPWA({
